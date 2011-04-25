abefdc5b69d202977a47cb354df7ae09ddb8c92f
Nekmo <contacto@nekmo.com>
2011-04-25 23:34:11 +0200

    Cambios varios en el sistema de rastreo de links y de la interfaz para su gestión

---
+++ fake_data_ddbb.py
@@ -0,0 +1,7 @@
+import hikkea.models as model
+
+form = model.Fansub(name='Nanikano', microname='NF')
+form.save()
+
+form = model.VideoCodec(name='Nanikano', microname='NF')
+form.save()
---
+++ templatetags/forms.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+import re
+register = template.Library()
+
+@register.filter(name='debug')
+def debug(value):
+    print(value)
+    print(type(value))
+    print(dir(value))
+
+@register.filter(name='items')
+def items(value):
+    to_del = ('npackage', value['npackage'])
+    items = value.items()
+    items.remove(to_del)
+    return items
+    
+@register.filter(name='namepackage')
+def namepackage(value):
+    # Se quita el partX
+    return re.sub('\.part\d+$|\.\d+$', '', value)
+
+#@register.filter(name='radio')
+#def radio(value):
+    #options = []
+    #print(dir(value))
+    #for key, value in value.field.widget.choices:
+        #pass
+        #'<label for="id_%(name)s">'\
+            #'<input type="radio" name="my_field"'\
+                   #'value="abc" id="id_my_field_x" />'\
+        #</label>'
